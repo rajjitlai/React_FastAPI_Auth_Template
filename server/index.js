3fa8f7ca60755e0f6730a24fd73089c2f0145779
RAJJIT <2.0lairaj@gmail.com>
2024-02-19 14:00:14 +0530

    config-02

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
+import cors from "cors"
 dotenv.config()
 import { UserRouter } from "./routes/User.js"
 
 const app = express()
 app.use(express.json())
+app.use(cors())
+
 app.use('/auth', UserRouter)
 
 mongoose.connect('mongodb://127.0.0.1:27017/authentication')
--- server/routes/User.js
+++ server/routes/User.js
@@ -5,13 +5,24 @@ const router = express.Router()
 
 import { User } from "../models/User.js";
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     const { username, email, password } = req.body;
 
-    const user = User.find({ email })
+    const user = await User.findOne({ email })
     if (user) {
         return res.json({ message: "User already existed" })
     }
+
+    const hashpassword = await bcrypt.hash(password, 10)
+
+    const newUser = new User({
+        username,
+        email,
+        password: hashpassword,
+    })
+
+    await newUser.save()
+    return res.json({ message: "record registered" })
 })
 
 export { router as UserRouter }
